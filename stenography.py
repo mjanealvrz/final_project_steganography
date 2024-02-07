71ccd2b2c3064fe2e9b09948cf10cec531664968
MJ Gillado <mjgillado0@gmail.com>
2024-02-07 18:07:31 +0800

    Made adjustments and added password entry

--- stenography.py
+++ stenography.py
@@ -11,7 +11,7 @@ from tkinter import messagebox
     # create window
 root=Tk()
 root.title("Log-in Page")
-root.geometry("925x600")
+root.geometry("975x600")
 root.configure(bg="#fff")
 root.resizable(False, False)
 
@@ -24,8 +24,20 @@ frame.place(x=600, y=100)
 heading= Label(frame, text="Sign-in", fg= "#57a1f8", bg="white",font=("Microsoft Yahei UI Light",23, "bold"))
 heading.place(x=100, y=5)
 
-user = Entry (frame, width= 25, fg="black", border=2, bg="white",font=("Microsoft Yahei UI Light",11) )
+user = Entry (frame, width= 25, fg="black", border=0, bg="white",font=("Microsoft Yahei UI Light",11) )
 user.place(x=30, y=80)
+user.insert(0, "Username")
+
+Frame(frame, width=295, height=2, bg="black").place(x=25, y=107)
+
+
+code = Entry (frame, width= 25, fg="black", border=0, bg="white",font=("Microsoft Yahei UI Light",11) )
+code.place(x=30, y=150)
+code.insert(0, "Password")
+
+Frame(frame, width=295, height=2, bg="black").place(x=25, y=177)
+
+
 
 
 
