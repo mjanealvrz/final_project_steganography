c2d5f1d893926f9261a873ca6aab5433c91b2c18
MJ Gillado <mjgillado0@gmail.com>
2024-02-07 18:15:00 +0800

    Sign-in button

--- stenography.py
+++ stenography.py
@@ -21,21 +21,27 @@ Label(root, image=img, bg="white").place(x=50, y=50)
 frame = Frame(root, width=350, height=350, bg="white")
 frame.place(x=600, y=100)
 
+# sign-in heading
 heading= Label(frame, text="Sign-in", fg= "#57a1f8", bg="white",font=("Microsoft Yahei UI Light",23, "bold"))
 heading.place(x=100, y=5)
 
+# username title
 user = Entry (frame, width= 25, fg="black", border=0, bg="white",font=("Microsoft Yahei UI Light",11) )
 user.place(x=30, y=80)
 user.insert(0, "Username")
 
 Frame(frame, width=295, height=2, bg="black").place(x=25, y=107)
 
-
+# password title
 code = Entry (frame, width= 25, fg="black", border=0, bg="white",font=("Microsoft Yahei UI Light",11) )
 code.place(x=30, y=150)
 code.insert(0, "Password")
 
 Frame(frame, width=295, height=2, bg="black").place(x=25, y=177)
+
+ 
+# sign-in button
+Button(frame, width=39, pady=7, text="Sign-in", bg= "#57a1f8", fg="white", border=0).place(x=35, y=204)
 
 
 
