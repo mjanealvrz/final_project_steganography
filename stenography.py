2e749a0486d3b696041618423903ddcaa36eabfa
MJ Gillado <mjgillado0@gmail.com>
2024-02-07 17:49:24 +0800

    Added image

--- stenography.py
+++ stenography.py
@@ -11,11 +11,14 @@ from tkinter import messagebox
     # create window
 root=Tk()
 root.title("Log-in Page")
-root.geometry("925x500")
+root.geometry("925x600")
 root.configure(bg="#fff")
 root.resizable(False, False)
 
+img = PhotoImage(file="login.png")
+Label(root, image=img, bg="white").place(x=50, y=50)
 root.mainloop()
+
 
 
 # Create the stenography using tkinter
