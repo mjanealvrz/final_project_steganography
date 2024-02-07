cb74836220cf275dea87a5bdc438f980db73a215
MJ Gillado <mjgillado0@gmail.com>
2024-02-07 17:08:09 +0800

    Added font style variables

--- stenography.py
+++ stenography.py
@@ -12,8 +12,17 @@ import bcrypt
 from tkinter import*
 from tkinter import messagebox
 
+# create window
 app = customtkinter.CTk()
 app.title("Log-in Page")
 app.geometry("450x360")
 app.config(bg="#001220")
+
+font1 = ("Helvetica", 25, "bold")
+font2 = ("Arial", 17, "bold")
+font3 = ("Arial", 13, "bold")
+font4 = ("Arial", 13, "bold", "underline")
+
+app.mainloop()
+
 
